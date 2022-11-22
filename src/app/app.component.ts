2bbe665a6fb71da27ffe80d8ac29ba7d82e83b6c
Gregory Saimoen <greg.eres.07@hotmail.com>
2022-11-22 20:41:44 +1100

    first commmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
+/**
+ * @title Highlight the first autocomplete option
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  myControl = new FormControl('');
+  options: string[] = [
+    'Nouméa',
+    'Belep',
+    'Boulouparis',
+    'Bourail',
+    'Canala',
+    'Dumbéa',
+    'Farino',
+    'Hienghène',
+    'Houaïlou',
+    'Île des Pins',
+    'Kaala-Gomen',
+    'Kouaoua',
+    'Koné',
+    'Koumac',
+    'La Foa',
+    'Lifou',
+    'Maré',
+    'Moindou',
+    'Mont-Dore',
+    'Ouégoa',
+    'Ouvéa',
+    'Païta',
+    'Poindimié',
+    'Ponérihouen',
+    'Pouembout',
+    'Pouébo',
+    'Poum',
+    'Poya',
+    'Sarraméa',
+    'Thio',
+    'Touho',
+    'Voh',
+    'Yaté',
+  ];
+  filteredOptions: Observable<string[]> | undefined;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value || ''))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    console.log(this.filteredOptions);
+
+    return this.options.filter((option) =>
+      option.toLowerCase().includes(filterValue)
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
